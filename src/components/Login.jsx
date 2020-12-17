eef4f78e182a59e21f9649b8dd49e6905f5bdf69
Nadav Aluf <nadav@champ.autos>
2020-12-17 07:43:41 +0200

    add axois

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,16 +3,12 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useHistory } from 'react-router-dom';
+import API from '../utils/API';
 
 const useStyles = makeStyles((theme) => ({
 	paper  : {
@@ -47,9 +43,15 @@ export default function SignIn (){
 		}
 		return setEmailIsValid(false);
 	};
-	const handleSubmit = (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
 		alert('Submitted form!');
+		let t = await API.post('/login', {
+			firstName,
+			lastName,
+			password,
+		});
+		debugger;
 		history.push('/Home');
 	};
 	return (
---
+++ src/utils/API.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL      : 'https://test-api-server.herokuapp.com/',
+	responseType : 'json',
+});
