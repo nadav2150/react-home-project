1bfa33684c40ffb0c46648b467215a3e76910d93
Nadav Aluf <nadav@champ.autos>
2020-12-22 19:24:05 +0200

    change isLodaing value

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -47,7 +47,7 @@ export default function SignIn (){
 				password,
 			});
 			history.push('/Home');
-			setIsLoading(true);
+			setIsLoading(false);
 		} catch (err) {
 			alert(`error is ${err}`);
 		}
